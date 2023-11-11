8f5ede499e86dba01c72a70e5d21a32e5f92840b
grayphantoms <khaledboxs@gmail.com>
2023-11-11 10:08:31 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -16,10 +16,10 @@ setuptools.setup(
     packages=setuptools.find_packages(),
     python_requires=r'>=3.8.0',
     install_requires=[
-        r'pytorch-lightning>=1.8.0',
-        r'fairscale>=0.4.3',
-        r'scikit-learn>=1.0.1',
-        r'pandas>=1.3.3',
+        r'pytorch-lightning',
+        r'fairscale',
+        r'scikit-learn',
+        r'pandas',
         r'hopfield-layers@git+https://github.com/ml-jku/hopfield-layers'
     ],
     zip_safe=True,
